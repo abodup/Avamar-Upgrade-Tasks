457f534e9dea7560a157ce30f03f99cf9aac9479
abodup <p.abdou99@gmail.com>
2017-09-21 11:49:27 +0200

    setupLog admin
    Initialized setupLog function

---
+++ upgrade_tasks_admin/setupLog.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from datetime import datetime
+def localTime():
+	return str(datetime.now())
+
+def setupLog():
+	global logfile 
+	
+	logfile = open("./upgrade_tasks_admin.log", "wa+")
+	
+setupLog()
+logfile.write("%s 321\n" %localTime())
+logfile.write("%s 654\n" %localTime())
+
+
