f027bfe94a3f06ba914a8bdca9a856d7fa0f8bcc
abodup <p.abdou99@gmail.com>
2017-09-22 20:22:33 +0200

    setuplog changes
    like previous commit

--- upgrade_tasks/Helpers/setupLog.py
+++ upgrade_tasks/Helpers/setupLog.py
@@ -3,6 +3,8 @@ import os
 ############### Start setupLog() ###############
 def setupLog():
 	global log 
+	os.system("touch upgrade_tasks.log")
+	os.system("chmod a+rw upgrade_tasks.log")
 	log = open("./upgrade_tasks.log", "wa")
 	text = """
 ##################################################################
