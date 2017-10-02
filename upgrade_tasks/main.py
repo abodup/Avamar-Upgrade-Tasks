b59e26bafff6ea91a3e7c6866f00001da6fc31e9
Peter <p.abdou99@gmail.com>
2017-10-02 11:18:22 +0200

    BackupMaintSched and helpers
    Adjusted logging for backupMantSched and adjusted helpers function quere_yes_no

--- upgrade_tasks/main.py
+++ upgrade_tasks/main.py
@@ -7,13 +7,13 @@ def main():
 		#verifyRevNo is correct
 		#verify Files are present
 		latestProactiveCheck()
-		os.system("sudo -u admin /home/admin/proactive_check/proactive_check.pl "+arg)
-		f = os.popen('sudo -u cat hc_results.txt')
-		print "Health checks Results\n ", f.read()
+		cmd("sudo -u admin /home/admin/proactive_check/proactive_check.pl --preupgrade=%s" %targetVersion)
+		output = cmdOut('sudo -u admin cat hc_results.txt')
+		printBoth("Health checks Results\n %s" output)
 		question = """Depending on the output of the health checks, if the health checks are clean press yes to continue
 if health checks are not clean press no to exit"""
 		if not query_yes_no(question): sys.exit()
-		#stopBackupMaintSched()
+		stopBackupMaintSched()
 		#extraChecks()
 
 
--- upgrade_tasks/stopBackupMaintSched.py
+++ upgrade_tasks/stopBackupMaintSched.py
@@ -10,44 +10,33 @@ def stopBackupMaintSched():
 	printLog(message)
 	# check if maintenence activities are running
 	printBoth("check if maintenence activities are running")
-	output = os.popen("status.dpn | egrep -A 2 checkpoint").read()
-	printLog("Command: status.dpn | egrep -A 2 checkpoint")
-	
+	output = cmdOut("sudo -u admin status.dpn | egrep -A 2 checkpoint")
 	lines = output.split('\n')
 	if lines[0][-3:-1] == 'OK':
-		print "checkpoint 'OK'"
+		printBoth("checkpoint 'OK'")
 	if lines[1][-3:-1] == 'OK':
-		print "GC 'OK'"
+		printBoth("GC 'OK'")
 	if lines[0][-3:-1] == 'OK':
-		print "hfscheck 'OK'"
+		printBoth("hfscheck 'OK'")
 	if (lines[0][-3:-1] == 'OK') and (lines[1][-3:-1] == 'OK') and (lines[2][-3:-1] == 'OK'):
-		print "Stopping Maintenence Scheduler"
-		os.system("dpnctl stop maint")
-	
-	output = os.popen("dpnctl status maint 2>&1").read()
-	lines = output.split('\n')
-	schedStatus = False
-	while lines[-2].split()[-1]!= 'suspended.':
+		printBoth("Stopping Maintenence Scheduler")
+		cmdOut("sudo -u admin dpnctl stop maint")
+	output = cmdOut("sudo -u admin dpnctl status maint 2>&1")
+	while output.split('\n')[-2].split()[-1]!= 'suspended.':
 		question = """couldn't stop the Maintenece scheduler
 please try manually and when done Press yes to continue or press no to quit"""
 		if not query_yes_no(question): sys.exit() 
-		output = os.popen("dpnctl status  2>&1").read()
-		lines = output.read().split('\n')	
+		output = cmdOut("sudo -u admin dpnctl status  2>&1")	
 	
-	os.system("dpnctl stop sched")
-	output = os.popen("dpnctl status sched 2>&1").read()
-	lines = output.split('\n')
-	schedStatus = False
-	while lines[-2].split()[-1]!= 'down.':
+	printBoth("Stopping Backup Scheduler")
+	cmdOut("sudo -u admin dpnctl stop sched")
+	output = cmdOut("sudo -u admin dpnctl status sched 2>&1")
+	while output.split('\n')[-2].split()[-1]!= 'down.':
 		question = """couldn't stop the Backup scheduler
 please try manually and when done Press yes to continue or press no to quit"""
 		if not query_yes_no(question): sys.exit() 
-		output = os.popen("dpnctl status  2>&1").read()
-		lines = output.split('\n')
-
-	
-	print "Backup and Maintenence scheduler are down"
-	
+		output = cmdOut("sudo -u admin dpnctl status sched 2>&1")
+		printBoth("Backup and Maintenence schedulers are down")
 	message ="""
 ##################################################################
 #                  End latestProactiveCheck                      #
